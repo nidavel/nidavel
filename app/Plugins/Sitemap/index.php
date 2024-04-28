2ec8496efe6bffbb1ec7ae5d5538bfe959780ecb
Ikennaf1 <iiikbros202@gmail.com>
2024-04-28 15:35:19 +0100

    Adds the sitemap plugin

--- app/Plugins/Sitemap/index.php
+++ app/Plugins/Sitemap/index.php
@@ -1,6 +1,16 @@
 <?php
 
+use Nidavel\Sitemap\Classes\Sitemap;
+
 require_once 'vendor/autoload.php';
 require_once plugin_path('Sitemap/routes/index.php');
 
 registerSettingsForm('Sitemap', 'sitemap', plugin_path('Sitemap/pages/settings.php'));
+
+function generateSitemap()
+{
+    $sitemap = Sitemap::getInstance();
+    $sitemap::generate();
+}
+
+runOnPostPublish('generateSitemap');
